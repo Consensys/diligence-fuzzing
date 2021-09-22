07b650ab7959392032b43f34571f7611dd5ac8d3
Joao Santos <jrmsantos15@gmail.com>
2021-09-22 15:57:00 +0100

    Bump version: 0.7.1 → 0.7.2

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ConsenSys/diligence-fuzzing",
-    version="0.7.1",
+    version="0.7.2",
     zip_safe=False,
 )
