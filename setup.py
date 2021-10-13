1078f86fe4f13b13761572ddc43905d79254ba1f
Joao Santos <jrmsantos15@gmail.com>
2021-10-13 13:08:53 +0100

    Bump version: 0.7.3 → 0.8.0

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ConsenSys/diligence-fuzzing",
-    version="0.7.3",
+    version="0.8.0",
     zip_safe=False,
 )
