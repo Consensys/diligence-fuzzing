e4dcd4e36fc586b827fdca778d1306d6261cbf69
Nuradil Alymkulov <nurchik17@gmail.com>
2022-08-24 15:08:40 +0600

    Bump version: 0.9.6 → 0.9.7

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ConsenSys/diligence-fuzzing",
-    version="0.9.6",
+    version="0.9.7",
     zip_safe=False,
 )
